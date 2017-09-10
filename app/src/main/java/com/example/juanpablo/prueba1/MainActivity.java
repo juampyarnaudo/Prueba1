be5d3028b10716181a8db3ebf30116614b60762f
Juan Pablo <iarnaudojuanpablo@gmail.com>
2017-09-10 20:02:52 -0300

    Modulo_Login

--- app/src/main/java/com/example/juanpablo/prueba1/MainActivity.java
+++ app/src/main/java/com/example/juanpablo/prueba1/MainActivity.java
@@ -1,63 +1,86 @@
 package com.example.juanpablo.prueba1;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView tvMessage;
+    private EditText etUser, etPassword;
+    private Button btnCreate, btnLogin;
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+    private String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        tvMessage = (TextView) findViewById(R.id.firebaseText);
+        etUser = (EditText) findViewById(R.id.etUser);
+        etPassword = (EditText) findViewById(R.id.etPassword);
+        btnCreate = (Button) findViewById(R.id.btnCreate);
+        btnLogin = (Button) findViewById(R.id.btnLogin);
+        mAuth = FirebaseAuth.getInstance();
 
-        FirebaseDatabase database = FirebaseDatabase.getInstance();
-        DatabaseReference myRef = database.getReference("message");
-        DatabaseReference myRef2 = database.getReference("Referencia 2");
-        DatabaseReference myRef3 = database.getReference("Referencia 3");
-        List<String> list = new ArrayList<>();
-        list.add("Juampy");
-        list.add("Maxi");
-        list.add("Paula");
-        list.add("Berni");
-
-
-        Map map = new HashMap();
-        map.put("hola", list);
-        map.put("hola1", "Lista");
-        map.put("hola2", list);
-
-        myRef2.setValue(list);
-        myRef3.setValue(map);
-
-        myRef.addValueEventListener(new ValueEventListener() {
+        btnCreate.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-                String value = dataSnapshot.getValue(String.class);
-                Log.d("MAIN ACTIVITY", "Value is: " + value);
-                tvMessage.setText(value);
+            public void onClick(View v) {
+                createUser();
             }
-
+            });
+        btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onCancelled(DatabaseError error) {
-                Log.w("MAIN ACTIVITY", "Failed to read value.", error.toException());
+            public void onClick(View v) {
+                loginUser();
             }
-        });
+            });
+
+    }
+
+    private void createUser() {
+        mAuth.createUserWithEmailAndPassword(etUser.getText().toString(),etPassword.getText().toString())
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        Log.d(TAG, "createUserWithEmail:onComplete:" + task.isSuccessful());
+
+                        if (!task.isSuccessful()) {
+                            Toast.makeText(getBaseContext(), "Error", Toast.LENGTH_SHORT).show();
+                        } else{
+                            Toast.makeText(getBaseContext(),"Correcto", Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
+    }
+    private void loginUser(){
+        mAuth.signInWithEmailAndPassword(etUser.getText().toString(),etPassword.getText().toString())
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        Log.d(TAG, "signInWithEmail:onComplete:" + task.isSuccessful());
+                        if (!task.isSuccessful()) {
+                            Log.w(TAG, "signInWithEmail:failed", task.getException());
+                            Toast.makeText(getBaseContext(), "Login incorrecto", Toast.LENGTH_SHORT).show();
+                        }   else {
+                            Toast.makeText(getBaseContext(), "Login correcto", Toast.LENGTH_SHORT).show();
+                        }
+
+                    }
+                });
+
     }
 }
