0bfe7abbfcb27d21574b168e550bff23b7a340b4
Maxi <maxi_ruiz140@hotmail.com>
2017-09-27 12:49:41 -0300

    Cargar imagen del usuario y guardarla en firebase

--- app/src/main/java/com/example/juanpablo/prueba1/MainActivity.java
+++ app/src/main/java/com/example/juanpablo/prueba1/MainActivity.java
@@ -1,14 +1,13 @@
 package com.example.juanpablo.prueba1;
 
 import android.content.Intent;
+import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.juanpablo.prueba1.activity.AccountActivity;
@@ -17,7 +16,6 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -25,7 +23,6 @@ public class MainActivity extends AppCompatActivity {
     private EditText etUser, etPassword;
     private Button btnCreate, btnLogin;
     private FirebaseAuth mAuth;
-    private FirebaseAuth.AuthStateListener mAuthListener;
     private String TAG = "MainActivity";
 
     @Override
